736c4db66652e920caeb4ca2445333e1c6b21133
Redver <116087243+Redver@users.noreply.github.com>
2025-04-09 14:16:18 +0200

    barcode scanner
    scan

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
-import ReactQRScanner from 'react-qr-scanner';
+import BarcodeScanner from './BarcodeScanner';
 
 function App() {
-  const [barcode, setBarcode] = useState('');
-
-  // Handle scan
-  const handleScan = (data) => {
-    if (data) {
-      setBarcode(data.text);
-      console.log('Scanned Barcode:', data.text);  // Log the scanned barcode
-    }
-  };
-
-  // Handle error
-  const handleError = (err) => {
-    console.error(err);
-  };
-
-  // Log facing mode when the component mounts
-  React.useEffect(() => {
-    console.log('Initializing QR scanner with back camera contraints (facingMode: environment)');
-  }, []);
-
   return (
-    <div className="App">
+    <div>
       <h1>Barcode Scanner</h1>
-      <div className="scanner">
-        <ReactQRScanner
-          delay={300}  // Delay in ms to optimize the scanning
-          constraints={{
-            facingMode: 'environment'
-        }}
-        key="environment"
-          onError={handleError}
-          onScan={handleScan}
-        />
-      </div>
-      <div className="result">
-        {barcode && <p>Scanned Barcode: {barcode}</p>}
-      </div>
+      <BarcodeScanner />
     </div>
   );
 }
---
+++ src/BarcodeScanner.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react";
+import { Html5Qrcode, Html5QrcodeScanType } from "html5-qrcode";
+
+const BarcodeScanner = () => {
+  const scannerRef = useRef(null);
+
+  useEffect(() => {
+    const config = {
+      fps: 10,
+      qrbox: { width: 250, height: 250 },
+      aspectRatio: 1.7777778,
+      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+    };
+
+    const html5QrCode = new Html5Qrcode("scanner");
+
+    Html5Qrcode.getCameras().then(devices => {
+      if (devices && devices.length) {
+        // Prefer rear camera if available
+        const rearCamera = devices.find(device => device.label.toLowerCase().includes("back")) || devices[0];
+
+        html5QrCode.start(
+          rearCamera.id,
+          config,
+          (decodedText, decodedResult) => {
+            console.log("✅ Code scanned:", decodedText, decodedResult);
+          },
+          (errorMessage) => {
+            console.log(errorMessage)
+          }
+        );
+      }
+    });
+
+    return () => {
+      html5QrCode.stop().then(() => html5QrCode.clear());
+    };
+  }, []);
+
+  return (
+    <div>
+      <div id="scanner" ref={scannerRef} style={{ width: "100%" }} />
+    </div>
+  );
+};
+
+export default BarcodeScanner;
